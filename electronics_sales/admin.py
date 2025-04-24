67b65c554b4c679db7609fcfd08fd24e7205ba1f
Egor Petruschenko <jaguar.94@mail.ru>
2025-04-24 16:29:57 +0600

    urlы, команда создания суперпользователя, админка

--- electronics_sales/admin.py
+++ electronics_sales/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from electronics_sales.models import Contacts, Product, SalesNetwork
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'country', 'city', 'street', 'house_number',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'model', 'release_date',)
+
+
+@admin.register(SalesNetwork)
+class SalesNetworkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'type_point', 'contacts', 'supplier', 'debt',)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,21 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    """Модель пользователя."""
+
+    username = models.CharField(max_length=150, unique=True, verbose_name='Ник')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    name = models.CharField(max_length=150, verbose_name='Имя', blank=True, null=True)
+    phone = models.CharField(max_length=15, verbose_name='Номер телефона', blank=True, null=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
