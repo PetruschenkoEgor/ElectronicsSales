d7b8fe138317bccb7b0cd439331eaf6481963463
Egor Petruschenko <jaguar.94@mail.ru>
2025-04-27 13:25:46 +0600

    фильтрация по стране

--- electronics_sales/urls.py
+++ electronics_sales/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
 
 from electronics_sales.apps import ElectronicsSalesConfig
-from electronics_sales.serializers import SalesNetworkSerializer
 from electronics_sales.views import SalesNetworkCreate, SalesNetworkUpdate, SalesNetworkList, SalesNetworkRetrieve, \
     SalesNetworkDestroy
 
 app_name = ElectronicsSalesConfig.name
 
 urlpatterns = [
-    path('sales-network-create/', SalesNetworkCreate.as_view(serializer_class=SalesNetworkSerializer), name='sales-network-create'),
-    path('<int:pk>/sales-network-update/', SalesNetworkUpdate.as_view(serializer_class=SalesNetworkSerializer), name='sales-network-update'),
-    path('sales-networks/', SalesNetworkList.as_view(serializer_class=SalesNetworkSerializer), name='sales-networks'),
-    path('<int:pk>/info-sales-network/', SalesNetworkRetrieve.as_view(serializer_class=SalesNetworkSerializer), name='info-sales-network'),
-    path('<int:pk>/destroy-sales-network/', SalesNetworkDestroy.as_view(serializer_class=SalesNetworkSerializer), name='destroy-sales-network'),
+    path('sales-network-create/', SalesNetworkCreate.as_view(), name='sales-network-create'),
+    path('<int:pk>/sales-network-update/', SalesNetworkUpdate.as_view(), name='sales-network-update'),
+    path('sales-networks/', SalesNetworkList.as_view(), name='sales-networks'),
+    path('<int:pk>/info-sales-network/', SalesNetworkRetrieve.as_view(), name='info-sales-network'),
+    path('<int:pk>/destroy-sales-network/', SalesNetworkDestroy.as_view(), name='destroy-sales-network'),
 ]
--- electronics_sales/views.py
+++ electronics_sales/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-
+from django_filters import rest_framework as filters
 from electronics_sales.models import SalesNetwork
 from electronics_sales.serializers import SalesNetworkSerializer
 
@@ -18,11 +18,23 @@ class SalesNetworkUpdate(generics.UpdateAPIView):
     serializer_class = SalesNetworkSerializer
 
 
+class SalesNetworkFilterCountry(filters.FilterSet):
+    """Фильтрация по стране."""
+
+    country = filters.CharFilter(field_name='contacts__country', lookup_expr='iexact')
+
+    class Meta:
+        model = SalesNetwork
+        fields = ['country']
+
+
 class SalesNetworkList(generics.ListAPIView):
     """Список звеньев сети продажи электроники."""
 
     queryset = SalesNetwork.objects.all()
     serializer_class = SalesNetworkSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = SalesNetworkFilterCountry
 
 
 class SalesNetworkRetrieve(generics.RetrieveAPIView):
